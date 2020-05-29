21a57fbf861c57b01afaf7fb4a38463993ed5329
flabberghasted1 <65828523+flabberghasted1@users.noreply.github.com>
2020-05-29 11:50:52 -0500

    .

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-let move = document.getElementById("move").innerHTML
-addEventListener(type = "click", document.getElementById("submit move"))
+move = ""
+let submit_move = document.getElementById("submit_move")
+submit_move.addEventListener(type = "click", function() {
+    let move = document.getElementById("move").innerHTML
+});
+if (move != "") {
+    if (move === "1"){
+        document.getElementById("sp1").innerHTML = ("")
+        document.getElementById("sp2").innerHTML = ("(o)")
+    }
+    if (move === "2"){
+        document.getElementById("sp1").innerHTML = ("")
+        document.getElementById("sp3").innerHTML = ("(o)")
+    }
+    if (move === "3"){
+        document.getElementById("sp1").innerHTML = ("")
+        document.getElementById("sp4").innerHTML = ("(o)")
+    }
+}
